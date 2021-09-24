d77d8d1527401c08b0c5252ad98b1bc10a17fe07
Ignacio Guerra <ignacioguerra@MacBook-Pro.localdomain>
2021-09-24 00:31:05 -0400

    Updating queries and adding new table from Skill Drill.

--- queries.sql
+++ queries.sql
@@ -44,3 +44,81 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info
+
+-- MODULE 7.3.2 Join Tables
+-- Recreate the retirement_info Table with the emp_no Column
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees and include emp_no column
+-- so that it can be joined later
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- MODULE 7.3.3 Joins in Action
+-- Use Inner Join for Departments and dept_manager Tables
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Use Left Join to Capture retirement_info Table
+-- Joining retirement_infor and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Using Aliases for Code Readability
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+-- Use Left Join for retirement_info and dept_emp tables and
+-- store in a new table called current_emp
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = '9999-01-01';
+
+-- MODULE 7.3.4 Use Count, Group By, and Order By
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+-- Perform the same select but now order the table by dept_no
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- SKILL DRILL - create a new table from the above select and export
+-- the table as csv.
+
